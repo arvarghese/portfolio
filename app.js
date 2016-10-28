986f845dbd0ba0536fd939f3cfdacafc721d58b3
Arun Varghese <avarghese@spireon.com>
2016-10-28 09:41:37 -0500

    load more details, added resume builder

--- app.js
+++ app.js
@@ -14,12 +14,20 @@ var portfolioDetails = {
 	"blog":{
 		"url":"http://avarghese.me/blog",
 		"techstack":['Github','HTML','Javascript','CSS'],
-		"description":"This is my personal blog created using Jekyll and Github"
+		"description":"This is my personal blog created using Jekyll and Github",
+		"blog":"http://avarghese.me/blog/development/2016/04/28/jekyll-blog.html"
 	},
 	"meteor":{
 		"url":"http://av.meteorapp.com",
 		"techstack":['Github','HTML','Javascript','CSS'],
-		"description":"This is my personal site built using Meteor.js"
+		"description":"This is my personal site built using Meteor.js",
+		"blog":"http://avarghese.me/blog/development/2016/01/01/meteor-website.html"
+	},
+	"resumebuilder":{
+		"url":"https://resume-builder5.herokuapp.com/",
+		"techstack":['Node','Angular 1','Heroku','HTML','Javascript','CSS'],
+		"description":"This is a demo app created using Angular 1, Node, Heroku",
+		"blog":"http://avarghese.me/blog/development/2016/06/01/first-mean-project.html"
 	}
 };
 
@@ -44,6 +52,7 @@ var Portfolio = {
 		$('#site').text(item.url);
 		$('#techstack').text(item.techstack);
 		$('#description').text(item.description);
+		$('#blog').html('<a href="'+item.blog+'" target="_blank">Link to Blog Post</a>');
 	}	
 };
 
